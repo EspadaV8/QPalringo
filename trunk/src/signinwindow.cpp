7f71d442f4f2cd705760b8ede22bc4d2be7e56f1
Andrew Smith <espadav8@gmail.com>
2008-10-21 21:04:43 +0000

    Add the GPL header text to all the cpp and h files, also include a copy of the GPL3 and a README outlining the use of the Palringo name and other external references (i.e. KDE4 icons and libPalringo).

--- trunk/src/signinwindow.cpp
+++ trunk/src/signinwindow.cpp
@@ -1,14 +1,23 @@
-//
-// C++ Implementation: signinwindow
-//
-// Description:
-//
-//
-// Author: Andrew Smith <espadav8@gmail.com>, (C) 2008
-//
-// Copyright: See COPYING file that comes with this distribution
-//
-//
+/***************************************************************************
+ *   Copyright (C) 2008 by Andrew Smith                                    *
+ *   espadav8@gmail.com                                                    *
+ *                                                                         *
+ *   This file is part of QPalringo                                        *
+ *                                                                         *
+ *  QPalringo is free software: you can redistribute it and/or modify      *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  QPalringo is distributed in the hope that it will be useful,           *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with QPalringo. If not, see <http://www.gnu.org/licenses/>       *
+ *                                                                         *
+ ***************************************************************************/
 #include <QSettings>
 #include "signinwindow.h"
 #include "tools.h"
