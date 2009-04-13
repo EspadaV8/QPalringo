3d7f7ab6a8a1ae24caf21f9e3dbc65571d3184bc
Andrew Smith <espadav8@gmail.com>
2009-04-13 20:35:10 +0000

    Missed a file when commiting the enums.

---
+++ branches/libQPalringo/src/libQPalringo/qpheaderattributes.h
@@ -0,0 +1,30 @@
+#ifndef QPHEADERATTRIBUTES_H
+#define QPHEADERATTRIBUTES_H
+
+
+namespace qpHeaderAttribute
+{
+    static const QString MESG_ID = "MESG-ID";
+    static const QString MESG_TARGET = "MESG-TARGET";
+    static const QString CORRELATION_ID = "CORRELATION-ID";
+    static const QString CONTENT_LENGTH  = "CONTENT-LENGTH";
+    static const QString TOTAL_LENGTH = "TOTAL-LENGTH";
+    static const QString TARGET_ID = "TARGET-ID";
+    static const QString SOURCE_ID = "SOURCE-ID";
+    static const QString CONTENT_TYPE = "CONTENT-TYPE";
+    static const QString EMAIL = "EMAIL";
+    static const QString NAME = "NAME";
+    static const QString TIMESTAMP = "TIMESTAMP";
+    static const QString LAST = "LAST";
+    static const QString HIST = "HIST";
+    static const QString COUNT = "COUNT";
+    static const QString FROM_GROUP = "FROM-GROUP";
+    static const QString FROM_PRIVATE = "FROM-PRIVATE";
+    static const QString SUB_ID = "SUB-ID";
+    static const QString NICKNAME = "NICKNAME";
+    static const QString STATUS = "STATUS";
+    static const QString LAST_ONLINE = "LAST-ONLINE";
+    static const QString REASON = "REASON";
+}
+
+#endif // QPHEADERATTRIBUTES_H
