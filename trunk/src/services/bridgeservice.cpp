b138540b448638ce6e094ab15df85be2f050b2f6
Andrew Smith <espadav8@gmail.com>
2009-05-03 23:08:33 +0000

    Add the offline icons to the bridge service (we currently always return true, since we have no way of enabling them).

--- trunk/src/services/bridgeservice.cpp
+++ trunk/src/services/bridgeservice.cpp
@@ -7,21 +7,48 @@ BridgeService::BridgeService()
 
 QString BridgeService::getIcon() const
 {
-    switch( this->type )
+    if( true )
     {
-        case qpBridgeType::MSN:
-            return ":/services/msn.svg";
-        case qpBridgeType::YAHOO:
-            return ":/services/yahoo.svg";
-        case qpBridgeType::AIM:
-            return ":/services/aim.svg";
-        case qpBridgeType::GADUGADU:
-            return ":/services/gaduGadu.svg";
-        case qpBridgeType::ICQ:
-            return ":/services/icq.svg";
-        case qpBridgeType::XMPP:
-            return ":/services/jabber.svg";
-        default:
-            return ":/svg/logo.svg";
+        switch( this->type )
+        {
+            case qpBridgeType::MSN:
+                return ":/services/msn_offline.svg";
+            case qpBridgeType::YAHOO:
+                return ":/services/yahoo_offline.svg";
+            case qpBridgeType::AIM:
+                return ":/services/aim_offline.svg";
+            case qpBridgeType::GADUGADU:
+                return ":/services/gaduGadu_offline.svg";
+            case qpBridgeType::ICQ:
+                return ":/services/icq_offline.svg";
+            case qpBridgeType::XMPP:
+                return ":/services/jabber_offline.svg";
+            case qpBridgeType::QQ:
+                return ":/services/qq_offline.svg";
+            default:
+                return ":/svg/logo_offline.svg";
+        }
+    }
+    else
+    {
+        switch( this->type )
+        {
+            case qpBridgeType::MSN:
+                return ":/services/msn.svg";
+            case qpBridgeType::YAHOO:
+                return ":/services/yahoo.svg";
+            case qpBridgeType::AIM:
+                return ":/services/aim.svg";
+            case qpBridgeType::GADUGADU:
+                return ":/services/gaduGadu.svg";
+            case qpBridgeType::ICQ:
+                return ":/services/icq.svg";
+            case qpBridgeType::XMPP:
+                return ":/services/jabber.svg";
+            case qpBridgeType::QQ:
+                return ":/services/qq.svg";
+            default:
+                return ":/svg/logo.svg";
+        }
     }
 }
