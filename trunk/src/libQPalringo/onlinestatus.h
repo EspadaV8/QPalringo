a15e7e4af068c7df86fad6c45926a29d43b84dff
Andrew Smith <espadav8@gmail.com>
2009-05-04 00:53:16 +0000

    Add a text match for each of the online status values.

--- trunk/src/libQPalringo/onlinestatus.h
+++ trunk/src/libQPalringo/onlinestatus.h
@@ -15,5 +15,19 @@ namespace qpOnlineStatus
         ON_PHONE,
         IDLE
     };
+
+    const QString OnlineStatusText[] =
+    {
+        "Offline",
+        "Online",
+        "Away",
+        "Invisible",
+        "Do not disturb",
+        "Busy",
+        "BRB",
+        "Out to lunch",
+        "On the phone",
+        "Idle"
+    };
 }
 #endif // ONLINESTATUS_H
