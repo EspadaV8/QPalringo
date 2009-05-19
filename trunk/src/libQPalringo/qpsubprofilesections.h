1a4abcbf967b9debed52a208f8e595fd0ac489b3
Andrew Smith <espadav8@gmail.com>
2009-05-19 18:25:44 +0000

    Add a file that'll contain all the sub profile sections that we know about.

---
+++ trunk/src/libQPalringo/qpsubprofilesections.h
@@ -0,0 +1,8 @@
+#ifndef QPSUBPROFILESECTION_H
+#define QPSUBPROFILESECTION_H
+namespace qpSubProfileSection
+{
+    static const QString CONTACTS = "contacts";
+    static const QString GROUPS = "group_sub";
+}
+#endif // QPSUBPROFILESECTION_H
