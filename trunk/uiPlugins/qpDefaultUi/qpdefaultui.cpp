e22aa79d4bd826ef4a2a3cdf6e5bcd8be101842a
Andrew Smith <espadav8@gmail.com>
2009-09-27 18:01:18 +0100

    Pass tools() to those that need it, connect up some signals

--- trunk/uiPlugins/qpDefaultUi/qpdefaultui.cpp
+++ trunk/uiPlugins/qpDefaultUi/qpdefaultui.cpp
@@ -1,31 +1,33 @@
 #include <QtGui>
 #include "qpdefaultui.h"
 #include "listviews/overviewlistview.h"
-//#include "tools.h"
+#include "listviews/contactlistview.h"
+#include "tools.h"
 
 QWidget* QPDefaultUi::getCentralWidget()
 {
     this->mainTabs = new QTabWidget();
-    /*
-    connect(mainTabs, SIGNAL(currentChanged(int)), this, SLOT(tabFocusChanged(int)));
-*/
-    OverviewListView *overviewList = new OverviewListView( mainTabs );
+
+    OverviewListView* overviewList = new OverviewListView( mainTabs, tools() );
     overviewList->setupContainers();
 
-    //mainTabs->addTab( overviewList, tools_->getPixmap( ":/svg/palringoService.svg" ), tr( "Overview" ) );
-    mainTabs->addTab( overviewList, tr( "Overview" ) );
-/*
-    ContactListView *contactList = new ContactListView( mainTabs );
+    mainTabs->addTab( overviewList, Tools::getPixmap( ":/svg/palringoService.svg" ), tr( "Overview" ) );
+
+    ContactListView* contactList = new ContactListView( mainTabs, tools() );
     contactList->setupContainers();
 
-    mainTabs->addTab( contactList, tools_->getPixmap( ":/svg/onlineContact.svg" ), tr( "&Contacts" ) );
-
-    setCentralWidget( mainTabs );
-
+    mainTabs->addTab( contactList, Tools::getPixmap( ":/svg/onlineContact.svg" ), tr( "&Contacts" ) );
+    /*
+    connect(mainTabs, SIGNAL(currentChanged(int)), this, SLOT(tabFocusChanged(int)));
     connect( tools_, SIGNAL( newGroupAdded( Group* )), this, SLOT( newGroupAdded( Group* ) ) );
     connect( tools_, SIGNAL( groupLeft( quint64 ) ), this, SLOT( groupLeft( quint64 ) ) );
     connect( tools_, SIGNAL( cleanUp() ), this, SLOT( cleanUp() ) );
     */
+
+    connect( overviewList, SIGNAL(signinPalringo(QString, QString )), tools(), SLOT( openPalringoConnection( QString, QString ) ));
+
+    //connect( tools_, SIGNAL( gotBridgeContact( BridgeContact* ) ), this, SLOT( bridgeContactReceived( BridgeContact* ) ) );
+
     return this->mainTabs;
 }
 
