7eee35a33665d83156328924ed0c052a3bf70d02
Andrew Smith <espadav8@gmail.com>
2009-09-30 22:31:40 +0100

    Change a `tools_` to `this` inside the Tools class

--- trunk/src/tools.cpp
+++ trunk/src/tools.cpp
@@ -117,7 +117,7 @@ void Tools::openPalringoConnection( QString email, QString password )
             connect( connection, SIGNAL( gotBridgeContact( BridgeContact* ) ), this, SIGNAL( gotBridgeContact( BridgeContact* ) ) );
             connect( connection, SIGNAL( messageReceived( Message ) ), this, SLOT( messageReceived( Message ) ) );
             connect( connection, SIGNAL( historyMessageReceived( Message ) ), this, SIGNAL( historyMessageReceived( Message ) ) );
-            connect( connection, SIGNAL( disconnected() ), tools_, SLOT( disconnected() ) );
+            connect( connection, SIGNAL( disconnected() ), this, SLOT( disconnected() ) );
         }
     }
 }
