0568bd0b214799a1f7ada75d92aea8e47a9ee913
Andrew Smith <espadav8@gmail.com>
2009-10-11 03:50:29 +0100

    Update the palringo service listing to add the contacts to the correct on/offline sections

--- uiPlugins/ircUi/panes/servicepanes/palringoservicepane.cpp
+++ uiPlugins/ircUi/panes/servicepanes/palringoservicepane.cpp
@@ -11,15 +11,26 @@ PalringoServicePane::PalringoServicePane( Service* service, QWidget* parent )
 void PalringoServicePane::updateContacts()
 {
     this->contacts = Tools::getContactListContacts();
-    this->contactList->clear();
 
     this->contactList->setUpdatesEnabled( false );
+
     foreach( Contact* contact, this->contacts )
     {
-        QTreeWidgetItem* twi = new QTreeWidgetItem( this->contactList );
+        QTreeWidgetItem* twi = new QTreeWidgetItem();
         twi->setText( 0, contact->getNickname() );
+        twi->setData( 1, Qt::DisplayRole, contact->getID() );
         twi->setIcon( 0, QPTools::getPixmap( QPTools::getTargetIcon( contact ) ) );
+        if( contact->getOnlineStatus() == qpOnlineStatus::OFFLINE )
+        {
+            this->offlineContactList->addChild( twi );
+        }
+        else
+        {
+            this->onlineContactList->addChild( twi );
+        }
     }
-    this->contactList->sortItems( 0, Qt::AscendingOrder );
+
+    this->onlineContactList->sortChildren( 0, Qt::AscendingOrder );
+    this->offlineContactList->sortChildren( 0, Qt::AscendingOrder );
     this->contactList->setUpdatesEnabled( true );
 }
