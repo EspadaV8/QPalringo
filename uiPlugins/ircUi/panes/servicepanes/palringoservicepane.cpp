a978f5d9c64a487a9a6e466f50854c9ec8525487
Andrew Smith <espadav8@gmail.com>
2009-10-11 03:26:54 +0100

    Implement the updateContacts() slot for the PalringoService (in a basic way)

--- uiPlugins/ircUi/panes/servicepanes/palringoservicepane.cpp
+++ uiPlugins/ircUi/panes/servicepanes/palringoservicepane.cpp
@@ -1,7 +1,25 @@
 #include "palringoservicepane.h"
+#include "tools.h"
+#include "qptools.h"
 
 PalringoServicePane::PalringoServicePane( Service* service, QWidget* parent )
     : ServicePane( service, parent )
 {
     //
+}
+
+void PalringoServicePane::updateContacts()
+{
+    this->contacts = Tools::getContactListContacts();
+    this->contactList->clear();
+
+    this->contactList->setUpdatesEnabled( false );
+    foreach( Contact* contact, this->contacts )
+    {
+        QTreeWidgetItem* twi = new QTreeWidgetItem( this->contactList );
+        twi->setText( 0, contact->getNickname() );
+        twi->setIcon( 0, QPTools::getPixmap( QPTools::getTargetIcon( contact ) ) );
+    }
+    this->contactList->sortItems( 0, Qt::AscendingOrder );
+    this->contactList->setUpdatesEnabled( true );
 }
--- uiPlugins/ircUi/panes/servicepanes/palringoservicepane.h
+++ uiPlugins/ircUi/panes/servicepanes/palringoservicepane.h
@@ -3,14 +3,20 @@
 
 #include "servicepane.h"
 #include "services/service.h"
+#include "targets/contact.h"
 
 class PalringoServicePane : public ServicePane
 {
+    Q_OBJECT
     public:
         PalringoServicePane( Service* service, QWidget* parent = 0 );
 
+    public slots:
+        virtual void updateContacts();
+
     private:
         Service* service;
+        QHash<quint64, Contact*> contacts;
 
 };
 
