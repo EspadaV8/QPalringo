981a86d1a19e60ffcd73f7f3d2c2b770c7b02cb0
espadav8 <espadav8@rock.localdomain>
2009-11-09 18:57:43 +0000

    Update the PalringoServicePane to use the new ContactTreeWidget when updating the contacts

--- uiPlugins/ircUi/panes/servicepanes/palringoservicepane.cpp
+++ uiPlugins/ircUi/panes/servicepanes/palringoservicepane.cpp
@@ -10,33 +10,7 @@ PalringoServicePane::PalringoServicePane( Service* service, QWidget* parent )
 
 void PalringoServicePane::updateContacts()
 {
-    bool onlineExpanded = this->onlineContactList->isExpanded();
-    bool offlineExpanded = this->offlineContactList->isExpanded();
-
-    this->contacts = Tools::getContactListContacts();
-
-    this->contactList->setUpdatesEnabled( false );
-
-    foreach( Contact* contact, this->contacts )
-    {
-        QTreeWidgetItem* twi = new QTreeWidgetItem();
-        twi->setText( 0, contact->getNickname() );
-        twi->setData( 1, Qt::DisplayRole, contact->getID() );
-        twi->setIcon( 0, QPTools::getPixmap( QPTools::getTargetIcon( contact ) ) );
-        if( contact->getOnlineStatus() == qpOnlineStatus::OFFLINE )
-        {
-            this->offlineContactList->addChild( twi );
-        }
-        else
-        {
-            this->onlineContactList->addChild( twi );
-        }
-    }
-
-    this->onlineContactList->sortChildren( 0, Qt::AscendingOrder );
-    this->offlineContactList->sortChildren( 0, Qt::AscendingOrder );
-
-    this->onlineContactList->setExpanded( onlineExpanded );
-    this->offlineContactList->setExpanded( offlineExpanded );
-    this->contactList->setUpdatesEnabled( true );
+    QHash<quint64, Contact*> contacts = Tools::getContactListContacts();
+    this->contactList->setContacts( contacts );
+    this->contactList->updateTree();
 }
